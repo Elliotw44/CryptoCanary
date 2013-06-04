a42c22b52fe07b17337f7dbcbe06e630fe614d73
Elliow44 <Elliotrex2@hotmail.com>
2013-06-03 22:48:20 -0700

    added comments to helper functions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-
+    #finds the current LTC price from BTC-e.com and returns it
     def ltc_price()
       @response = HTTParty.get("https://www.btc-e.com/api/2/ltc_usd/ticker")
       @JSON = JSON.parse(@response)
--- app/helpers/worker_helper.rb
+++ app/helpers/worker_helper.rb
@@ -1,5 +1,5 @@
 module WorkerHelper
-
+    #gets the pool stats from invasion network
     def invpool_stats(apikey)
         @response = HTTParty.get("http://www.invasionnetwork.com/api?api_key=#{apikey}")
         JSON.parse(@response)
