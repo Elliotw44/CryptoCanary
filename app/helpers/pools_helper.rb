f104e3189a673255e5545c4b6e2ea635b4fc78ae
Elliow44 <Elliotrex2@hotmail.com>
2013-06-14 16:32:40 -0700

    added more pool apis and more work on live updating

--- app/helpers/pools_helper.rb
+++ app/helpers/pools_helper.rb
@@ -1,2 +1,33 @@
 module PoolsHelper
+    
+    def invpool_stats(apikey)
+        @response = HTTParty.get("http://www.invasionnetwork.com/api?api_key=#{apikey}")
+        JSON.parse(@response)
+    end
+
+    def wemineltc_stats(apikey)
+        @response = HTTParty.get("http://www.wemineltc.com/api?api_key=#{apikey}")
+        JSON.parse(@response)
+    end
+
+    def givemeltc_stats(apikey)
+        @response = HTTParty.get("http://www.give-me-ltc.com/api?api_key=#{apikey}")
+        JSON.parse(@response)
+    end
+
+    def litecoinpool_stats(apikey)
+        @response = HTTParty.get("http://www.litecoinpool.org/api?api_key=#{apikey}")
+        JSON.parse(@response) 
+    end
+
+    def poolitzod_stat(apikey)
+         @response = HTTParty.get("http://pool.itzod.ru/apiex.php?act=getuserstats&key=#{apikey}")
+         JSON.parse(@response)
+    end
+
+    def hypernova_stats(apikey)
+        @response = HTTParty.get("https://hypernova.pw/api/key/#{apikey}/")
+        JSON.parse(@response)
+    end
+
 end
--- app/helpers/workers_helper.rb
+++ app/helpers/workers_helper.rb
@@ -1,2 +1,7 @@
 module WorkersHelper
+    def find_worker_owner_by_ip(ip)
+        @worker = Worker.find_by_ip_address(ip)
+        @user = User.find(@worker.user_id)
+    end
+    
 end
