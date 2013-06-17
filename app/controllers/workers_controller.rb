3b19e9330a08c19b63dafc7246534e7737b1056a
Elliow44 <Elliotrex2@hotmail.com>
2013-06-16 21:19:36 -0700

    changing living updating param

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -24,7 +24,7 @@ class WorkersController < ApplicationController
     def update
         @user = User.find(params[:user_id])
         @worker = Worker.find(params[:id])
-        @MHS = params[:SUMMARY][0][MHS av]
+        @MHS = params[:SUMMARY][0]["MHS av"]
         @worker.hashrate = @MHS
         @worker.save
     end
