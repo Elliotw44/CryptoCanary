2e7fbc43d9d575a23f37e3a3a973d4ce6bc37f2d
Elliot <elliotrex2@hotmail.com>
2013-06-17 10:25:12 -0700

    Removed duplicate files

--- app/helpers/worker_helper.rb
+++
@@ -1,8 +0,0 @@
-module WorkerHelper
-    #gets the pool stats from invasion network
-    def invpool_stats(apikey)
-        @response = HTTParty.get("http://www.invasionnetwork.com/api?api_key=#{apikey}")
-        JSON.parse(@response)
-    end
-
-end
