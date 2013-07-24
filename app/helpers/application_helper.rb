6184cd18fce5d2e92011d76e6df26e69ced699b4
Elliow44 <Elliotrex2@hotmail.com>
2013-07-23 20:09:48 -0700

    added new 5s avg total hashrate

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,12 @@ module ApplicationHelper
     def ltc_price()
       @response = HTTParty.get("https://www.btc-e.com/api/2/ltc_usd/ticker")
       @JSON = JSON.parse(@response)
-      @ticker = @JSON['ticker']
-      @price = @ticker['sell']
+      @price = @JSON['ticker']['sell']
     end
 
     def btc_price()
         @response = HTTParty.get("https://www.btc-e.com/api/2/btc_usd/ticker")
         @JSON = JSON.parse(@response)
-        @ticket = @JSON['ticker']
-        @price = @ticket['sell']
+        @price  = @JSON['ticker']['sell']
     end
 end
