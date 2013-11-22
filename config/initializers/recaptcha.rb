b98c9b4a03830dfb3bc5dd3b0711a05abc83833e
Elliow44 <Elliotrex2@hotmail.com>
2013-11-22 10:49:21 -0800

    White reCaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -2,5 +2,5 @@ Recaptcha.configure do |config|
     config.public_key = '6Lf3qOoSAAAAAPHqCQzegfrrSWChALwbNAOQZaa8'
     config.private_key = '6Lf3qOoSAAAAAEfje5fU8R_P7PPZKn6VrD6ql8m6'
     config.use_ssl_by_default
-    config.proxy= 'https://miningmonitor.herokuapp.com'
+    config.proxy = 'https://miningmonitor.herokuapp.com:8080'
 end
