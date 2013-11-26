23d16ba10b964d26e423871accc3c42bae0085a6
Elliow44 <Elliotrex2@hotmail.com>
2013-11-26 14:40:06 -0800

    Fix for Heroku reCaptcha

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,10 @@ MiningApp::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  ENV['RECAPTCHA_PUBLIC_KEY'] = '6Lfp6OoSAAAAAOGPQCY1ACJNfPRsExtwly7S0RX-'                                                                                                                
+  ENV['RECAPTCHA_PRIVATE_KEY'] = '6Lfp6OoSAAAAAGIwCDiWQKkuzVUuJ4xuziVGZzXd'
+  #
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
--- config/initializers/recaptcha.rb
+++
@@ -1,5 +0,0 @@
-Recaptcha.configure do |config|
-    config.public_key = '6Lfp6OoSAAAAAOGPQCY1ACJNfPRsExtwly7S0RX-'
-    config.private_key= '6Lfp6OoSAAAAAGIwCDiWQKkuzVUuJ4xuziVGZzXd'
-    config.use_ssl_by_default
-end
