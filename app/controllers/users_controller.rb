c28d4da1ac5b097862e9b1b1b0957da8ecb72508
Elliow44 <Elliotrex2@hotmail.com>
2013-11-26 15:47:31 -0800

    remove timeout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
         @user= User.new(params[:user])
         @user.name.strip!
         respond_to do |format|
-         if(verify_recaptcha(:model => @user, :message => "Error with reCaptcha!", :private_key => ENV['RECAPTCHA_PRIVATE_KEY'], :timeout => '10') && @user.save)
+         if(verify_recaptcha(:model => @user, :message => "Error with reCaptcha!", :private_key => ENV['RECAPTCHA_PRIVATE_KEY']) && @user.save)
               sign_in(@user)
               format.html { redirect_to(@user, flash[:success] = "Welcome to Miners Canary!") }
          else
