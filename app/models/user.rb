942705bb8a9507d525f6d76aebcb4eb659a27e45
Elliow44 <Elliotrex2@hotmail.com>
2013-11-26 15:31:23 -0800

    removing coin from login for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
-  validates :coins, presence: true, format: { with: VALID_COINS_REGEX }
+  #validates :coins, presence: true, format: { with: VALID_COINS_REGEX }
 
   private
     def create_remember_token
