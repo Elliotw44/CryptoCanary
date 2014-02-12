961c1d4f0e94ebbc3c865879ea7b96d060ba1ae7
Elliow44 <Elliotrex2@hotmail.com>
2014-02-11 16:46:40 -0800

    hope for fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 MiningApp::Application.routes.draw do
 
-  match '/workers/update', to: 'workers#update', via: :put
+  match '/workers/update', to: 'workers#update', via: :patch
   resources :users do
      resources :workers, only: [:new, :create, :destroy, :show, :edit]
   end 
